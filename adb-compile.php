a7faafd5ecf998918bb4cd8b12636e8d5bbfc575
chenlei <leilei1512159@gmail.com>
2020-05-09 12:08:03 +0800

    optimization

--- adb-compile.php
+++ adb-compile.php
@@ -56,7 +56,7 @@ if ($should_compile_num > 0) {
 
     echo '正在停止新编译应用程序...';
     for ($i = 0; $i < $should_compile_num; $i++) {
-        echo '当前进度: ' . $i . '/' . $should_compile_num, PHP_EOL;
+        echo '当前进度: ' . ($i + 1) . '/' . $should_compile_num, PHP_EOL;
         stopApp($should_compile[$i]);
     }
 
